d83753c04a551bba8f4cc7653ce9823954c37fd6
filbertik <aton2019zvit@gmail.com>
2026-01-22 12:00:00 +0200

    fix logo gap1

--- src/config/site.config.ts
+++ src/config/site.config.ts
@@ -2,8 +2,9 @@ export const siteConfig = {
   title: "Tech Hive",
   description: "Tech Shop",
   navItems: [
-    { href: "/", label: "Акції" },
-    { href: "/ingredients", label: "Контакти" },
+    { href: "/", label: "Каталог" },
+    { href: "/ingredients", label: "Акції" },
+    { href: "/contacts", label: "Контакти" },
     { href: "/about", label: "Доставка" },
   ],
   pagesContent: {
@@ -12,6 +13,33 @@ export const siteConfig = {
     },
     "/ingredients": {
       content: "Дані...",
+    },
+    "/contacts": {
+      content: `
+        <p>
+          Опис
+        </p>
+          <br/>
+        <h2>Опис</h2>
+          <br/
+         <ul>
+          <li>
+            <strong>Опис</strong> - Опис
+          </li>
+          <li>
+            <strong>Опис</strong> - Опис
+          </li>
+          <li>
+            <strong>Опис/strong> - Опис
+          </li>
+          <li>
+            <strong>Опис</strong> - Опис
+          </li>
+          <li>
+            <strong>Опис</strong> - Опис
+          </li>
+        </ul>
+      `,
     },
     "/about": {
       content: `
