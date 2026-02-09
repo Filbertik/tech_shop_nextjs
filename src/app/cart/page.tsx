afbea4fd3b5bea41cceeddde5402bf7348eefc65
filbertik <aton2019zvit@gmail.com>
2026-02-09 12:00:00 +0200

    add page template cat

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={ padding: "40px" }>
+      <h1>Shopping Cart</h1>
+      <p>Це заготовка сторінки "Shopping Cart".</p>
+    </div>
+  );
+}
