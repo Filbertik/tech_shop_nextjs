90f96ff206304ac0ebd59c9c11f6a4205076dbcc
filbertik <aton2019zvit@gmail.com>
2026-02-10 12:00:00 +0200

    add catlog

---
+++ src/app/catalog/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={ padding: "40px" }>
+      <h1>Catalog Page</h1>
+      <p>Це заготовка сторінки "Catalog Page".</p>
+    </div>
+  );
+}
