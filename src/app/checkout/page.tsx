13992c3864dc9b99123b5ae5e4bc62f594252114
filbertik <aton2019zvit@gmail.com>
2026-02-12 12:00:00 +0200

    add checkout

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={ padding: "40px" }>
+      <h1>Checkout Page</h1>
+      <p>Це заготовка сторінки "Checkout Page".</p>
+    </div>
+  );
+}
