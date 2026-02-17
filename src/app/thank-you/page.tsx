b03c8c53b470f77eb55eb8f4b5cc4bb99be4f451
filbertik <aton2019zvit@gmail.com>
2026-02-17 12:00:00 +0200

    add thanks

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={ padding: "40px" }>
+      <h1>Thank You Page</h1>
+      <p>Це заготовка сторінки "Thank You Page".</p>
+    </div>
+  );
+}
