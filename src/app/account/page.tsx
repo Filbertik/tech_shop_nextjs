3f52e4d970e34331574276fa10f21657308a37b4
filbertik <aton2019zvit@gmail.com>
2026-02-18 12:00:00 +0200

    add thank

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,6 +1,8 @@
 export default function Page() {
   return (
-    <div style={ padding: "40px" }>
+    <div style={{ padding: "40px" }}>
+      {/* <div style={ padding: "40px" }> */}
+
       <h1>Account Page</h1>
       <p>Це заготовка сторінки "Account Page".</p>
     </div>
