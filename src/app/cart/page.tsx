72a8f8157ae4a8091b84b5c9bcd409d09794dee5
filbertik <aton2019zvit@gmail.com>
2026-02-19 12:00:00 +0200

    edit cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
   return (
-    <div style={ padding: "40px" }>
+    <div style={{ padding: "40px" }}>
       <h1>Shopping Cart</h1>
       <p>Це заготовка сторінки "Shopping Cart".</p>
     </div>
