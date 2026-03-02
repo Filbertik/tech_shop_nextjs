8500b91d934b9ac1ac26d5056f34cd73db2e6473
filbertik <aton2019zvit@gmail.com>
2026-03-02 12:00:00 +0200

    edit thank

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
   return (
-    <div style={ padding: "40px" }>
+    <div style={{ padding: "40px" }}>
       <h1>Thank You Page</h1>
       <p>Це заготовка сторінки "Thank You Page".</p>
     </div>
