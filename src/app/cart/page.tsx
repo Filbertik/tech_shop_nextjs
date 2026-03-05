0519ddc252482e7b0847924cfc5dce822193bf79
filbertik <aton2019zvit@gmail.com>
2026-03-05 12:00:00 +0200

    edit-

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <div style={{ padding: "40px" }}>
       <h1>Shopping Cart</h1>
-      <p>Це заготовка сторінки "Shopping Cart".</p>
+      <p>`Це заготовка сторінки Shopping Cart.`</p>
     </div>
   );
 }
