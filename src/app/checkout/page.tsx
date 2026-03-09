4f2bcfa7b730e2d933fdd09fee440d5d7cba6974
filbertik <aton2019zvit@gmail.com>
2026-03-09 12:00:00 +0200

    edit-

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <div style={{ padding: "40px" }}>
       <h1>Checkout Page</h1>
-      <p>Це заготовка сторінки "Checkout Page".</p>
+      <p>`Це заготовка сторінки Checkout Page.`</p>
     </div>
   );
 }
