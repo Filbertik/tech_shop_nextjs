b2965f038b60805bfec795820af374ac9fb4138f
filbertik <aton2019zvit@gmail.com>
2026-03-25 12:00:00 +0200

    add home components

---
+++ src/components/home/choice.tsx
@@ -0,0 +1,8 @@
+export default function Choice() {
+  return (
+    <section>
+      <h2 className="text-2xl font-semibold mb-4">Our Choice</h2>
+      <p>Рекомендовані товари.</p>
+    </section>
+  );
+}
