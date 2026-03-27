39f68d1d8b4e0f9c9e329bc63814a18ee8f50067
filbertik <aton2019zvit@gmail.com>
2026-03-27 12:00:00 +0200

    add home components

---
+++ src/components/home/sale.tsx
@@ -0,0 +1,8 @@
+export default function Sale() {
+  return (
+    <section className="bg-red-50 p-6 rounded-2xl">
+      <h2 className="text-2xl font-semibold mb-4">Sale</h2>
+      <p>Товари зі знижками.</p>
+    </section>
+  );
+}
