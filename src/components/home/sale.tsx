e46e9f6f5b00d73978741a96abd7a134286b8b9b
filbertik <aton2019zvit@gmail.com>
2026-05-11 12:00:01 +0300

    sales

--- src/components/home/sale.tsx
+++ src/components/home/sale.tsx
@@ -152,12 +152,3 @@ export default function Bestsellers() {
     </section>
   );
 }
-
-// export default function Sale() {
-//   return (
-//     <section className="bg-red-50 p-6 rounded-2xl">
-//       <h2 className="text-2xl font-semibold mb-4">Sale</h2>
-//       <p>Товари зі знижками.</p>
-//     </section>
-//   );
-// }
