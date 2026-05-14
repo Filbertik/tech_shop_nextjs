5542f2e8f22a219068e564bfb4a8d79c21b51228
filbertik <aton2019zvit@gmail.com>
2026-05-14 16:03:19 +0300

    form clear

--- src/components/home/form.tsx
+++ src/components/home/form.tsx
@@ -153,11 +153,3 @@ export default function FormSection() {
     </section>
   );
 }
-// export default function FormSection() {
-//   return (
-//     <section className="bg-gray-100 p-6 rounded-2xl">
-//       <h2 className="text-2xl font-semibold mb-4">Stay in touch</h2>
-//       <p>Форма підписки або зворотного зв’язку.</p>
-//     </section>
-//   );
-// }
