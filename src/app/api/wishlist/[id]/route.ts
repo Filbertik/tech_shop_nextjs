56f45c51ef977e3d894adb808e1758e9848a44d2
filbertik <aton2019zvit@gmail.com>
2026-06-10 11:03:23 +0300

    fix  for versel

--- src/app/api/wishlist/[id]/route.ts
+++ src/app/api/wishlist/[id]/route.ts
@@ -2,10 +2,14 @@ import { NextResponse } from "next/server";
 import prisma from "@/utils/prisma";
 import { getCurrentUser } from "@/lib/getCurrentUser";
 
+export const runtime = "nodejs";
+
 export async function DELETE(
   req: Request,
-  { params }: { params: { id: string } },
+  context: { params: Promise<{ id: string }> },
 ) {
+  const { id } = await context.params;
+
   const user = await getCurrentUser();
 
   if (!user) {
@@ -14,7 +18,7 @@ export async function DELETE(
 
   await prisma.favorite.delete({
     where: {
-      id: params.id,
+      id,
     },
   });
 
@@ -43,3 +47,26 @@ export async function DELETE(
 
 //   return NextResponse.json({ success: true });
 // }
+
+// // import { NextResponse } from "next/server";
+// // import prisma from "@/utils/prisma";
+// // import { getCurrentUser } from "@/lib/getCurrentUser";
+
+// // export async function DELETE(
+// //   req: Request,
+// //   { params }: { params: { id: string } },
+// // ) {
+// //   const user = await getCurrentUser();
+
+// //   if (!user) {
+// //     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+// //   }
+
+// //   await prisma.favorite.delete({
+// //     where: {
+// //       id: params.id,
+// //     },
+// //   });
+
+// //   return NextResponse.json({ success: true });
+// // }
