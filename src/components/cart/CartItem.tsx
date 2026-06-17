56ea912cc2d6de66da7f9c26251e55c561c54fe2
filbertik <aton2019zvit@gmail.com>
2026-06-17 10:22:45 +0300

    fix type for versel

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -3,7 +3,17 @@
 import Image from "next/image";
 import { useCartStore } from "@/store/cart.store";
 
-export default function CartItem({ item }: any) {
+// ✅ ТИП ЗАМІСТЬ any
+type CartItemType = {
+  id: string;
+  title: string;
+  image: string;
+  price: number;
+  oldPrice?: number;
+  quantity: number;
+};
+
+export default function CartItem({ item }: { item: CartItemType }) {
   const { increase, decrease, remove } = useCartStore();
 
   return (
@@ -60,3 +70,66 @@ export default function CartItem({ item }: any) {
     </div>
   );
 }
+
+// "use client";
+
+// import Image from "next/image";
+// import { useCartStore } from "@/store/cart.store";
+
+// export default function CartItem({ item }: any) {
+//   const { increase, decrease, remove } = useCartStore();
+
+//   return (
+//     <div className="flex w-[510px] h-[120px] gap-3">
+//       {/* IMAGE */}
+//       <div className="w-[120px] h-[120px] relative">
+//         <Image
+//           src={item.image}
+//           alt={item.title}
+//           fill
+//           className="object-contain rounded"
+//         />
+//       </div>
+
+//       {/* INFO */}
+//       <div className="w-[223px] flex flex-col justify-between">
+//         <p className="text-[16px] line-clamp-2">{item.title}</p>
+
+//         {/* COUNTER */}
+//         <div className="flex items-center gap-2">
+//           <button onClick={() => decrease(item.id)}>
+//             <Image src="/images/cat/minus.svg" alt="-" width={16} height={16} />
+//           </button>
+
+//           <span>{item.quantity}</span>
+
+//           <button onClick={() => increase(item.id)}>
+//             <Image src="/images/cat/plus.svg" alt="+" width={16} height={16} />
+//           </button>
+//         </div>
+//       </div>
+
+//       {/* PRICE */}
+//       <div className="flex flex-col justify-start">
+//         {item.oldPrice && (
+//           <span className="text-[12px] line-through text-gray-400">
+//             {item.oldPrice} ₴
+//           </span>
+//         )}
+//         <span className="text-[20px] font-semibold">
+//           {item.price * item.quantity} ₴
+//         </span>
+//       </div>
+
+//       {/* REMOVE */}
+//       <button onClick={() => remove(item.id)}>
+//         <Image
+//           src="/images/cat/close.svg"
+//           alt="remove"
+//           width={16}
+//           height={16}
+//         />
+//       </button>
+//     </div>
+//   );
+// }
