6e913670eeb3809f5ab46bc1453d8d1603bfc7c5
filbertik <aton2019zvit@gmail.com>
2026-06-19 16:13:23 +0300

    order ch add payment method

---
+++ src/components/checkout/PaymentMethod.tsx
@@ -0,0 +1,9 @@
+export default function PaymentMethod() {
+  return (
+    <div className="w-full h-[543px] rounded-[4px] p-[40px_24px] shadow-[0_9px_22px_rgba(21,53,90,0.1)] bg-white">
+      <h2 className="text-[18px] font-semibold mb-4">Спосіб оплати</h2>
+
+      {/* тут буде вибір оплати */}
+    </div>
+  );
+}
