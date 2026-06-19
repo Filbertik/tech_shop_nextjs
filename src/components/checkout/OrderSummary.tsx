79184dfa1b59b8012a867a872ca1c00e69f4deb2
filbertik <aton2019zvit@gmail.com>
2026-06-19 16:16:26 +0300

    order ch add order symmary

---
+++ src/components/checkout/OrderSummary.tsx
@@ -0,0 +1,9 @@
+export default function OrderSummary() {
+  return (
+    <div className="w-full min-h-[433px] rounded-[4px] p-[24px] shadow-[0_9px_22px_rgba(21,53,90,0.1)] bg-white">
+      <h2 className="text-[18px] font-semibold mb-4">Ваше замовлення</h2>
+
+      {/* тут буде список товарів + сума */}
+    </div>
+  );
+}
