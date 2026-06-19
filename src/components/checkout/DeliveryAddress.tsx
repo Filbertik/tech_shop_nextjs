38a600a0603c948183391c76b2a2fdd1671e050a
filbertik <aton2019zvit@gmail.com>
2026-06-19 16:12:33 +0300

    order ch add deliveryaddress

---
+++ src/components/checkout/DeliveryAddress.tsx
@@ -0,0 +1,9 @@
+export default function DeliveryAddress() {
+  return (
+    <div className="w-full h-[324px] rounded-[4px] p-[40px_24px] shadow-[0_9px_22px_rgba(21,53,90,0.1)] bg-white">
+      <h2 className="text-[18px] font-semibold mb-4">Адрес доставки</h2>
+
+      {/* тут буде форма */}
+    </div>
+  );
+}
