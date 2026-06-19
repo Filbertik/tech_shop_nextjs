2efd6cc43fcaead636d6516323806ecf114c336b
filbertik <aton2019zvit@gmail.com>
2026-06-19 16:11:36 +0300

    order checkout add contact info

---
+++ src/components/checkout/ContactInfo.tsx
@@ -0,0 +1,9 @@
+export default function ContactInfo() {
+  return (
+    <div className="w-full h-[395px] rounded-[4px] p-[40px_24px] shadow-[0_9px_22px_rgba(21,53,90,0.1)] bg-white">
+      <h2 className="text-[18px] font-semibold mb-4">Контактна інформація</h2>
+
+      {/* тут буде форма */}
+    </div>
+  );
+}
