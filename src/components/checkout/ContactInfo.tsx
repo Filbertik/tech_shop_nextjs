9ebbfaa701eb604294668e0dd419da5d629da5cb
filbertik <aton2019zvit@gmail.com>
2026-06-20 12:25:19 +0300

    order ch add contact info  inputs

--- src/components/checkout/ContactInfo.tsx
+++ src/components/checkout/ContactInfo.tsx
@@ -1,9 +1,49 @@
+import { Input } from "@heroui/react";
+
 export default function ContactInfo() {
   return (
     <div className="w-full h-[395px] rounded-[4px] p-[40px_24px] shadow-[0_9px_22px_rgba(21,53,90,0.1)] bg-white">
       <h2 className="text-[18px] font-semibold mb-4">Контактна інформація</h2>
 
       {/* тут буде форма */}
+      {/* BLOCK 1 */}
+      <div className="w-[595px]">
+        {/* <h2 className="font-semibold text-[18px] mb-[24px]">Дані акаунта</h2> */}
+
+        <div className="flex flex-col gap-[16px]">
+          <Input
+            name="firstName"
+            placeholder="Ім’я"
+            // value={form.firstName}
+            // onChange={handleChange}
+            // error={errors.firstName}
+          />
+
+          <Input
+            name="lastName"
+            placeholder="Прізвище"
+            // value={form.lastName}
+            // onChange={handleChange}
+            // error={errors.lastName}
+          />
+
+          <Input
+            name="phone"
+            placeholder="Номер телефону"
+            // value={form.phone}
+            // onChange={handleChange}
+            // error={errors.phone}
+          />
+
+          <Input
+            name="email"
+            placeholder="Email"
+            // value={form.email}
+            // onChange={handleChange}
+            // error={errors.email}
+          />
+        </div>
+      </div>
     </div>
   );
 }
