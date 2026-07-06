b925b3adf8fa1151dc5a550141bd6329dff1011c
filbertik <aton2019zvit@gmail.com>
2026-07-06 13:44:41 +0300

    order ch add deliveryadress content

--- src/components/checkout/DeliveryAddress.tsx
+++ src/components/checkout/DeliveryAddress.tsx
@@ -1,9 +1,40 @@
+import { Input } from "@heroui/react";
+
 export default function DeliveryAddress() {
   return (
     <div className="w-full h-[324px] rounded-[4px] p-[40px_24px] shadow-[0_9px_22px_rgba(21,53,90,0.1)] bg-white">
-      <h2 className="text-[18px] font-semibold mb-4">Адрес доставки</h2>
+      {/* 👉 ГОЛОВНИЙ КОНТЕЙНЕР */}
+      <div className="flex flex-col gap-[40px]">
+        <h2 className="text-[18px] font-semibold">Адрес доставки</h2>
 
-      {/* тут буде форма */}
+        {/* 👉 ФОРМА */}
+        <div className="flex flex-col gap-[16px] w-[595px]">
+          {/* Місто */}
+          <Input placeholder="Місто" />
+
+          {/* Вулиця */}
+          <Input placeholder="Вулиця" />
+
+          {/* 👉 РЯД З 3 ІНПУТАМИ */}
+          <div className="flex gap-[16px]">
+            <Input placeholder="Дім" className="flex-1" />
+
+            <Input placeholder="Квартира" className="flex-1" />
+
+            <Input placeholder="Поверх" className="flex-1" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
+// export default function DeliveryAddress() {
+//   return (
+//     <div className="w-full h-[324px] rounded-[4px] p-[40px_24px] shadow-[0_9px_22px_rgba(21,53,90,0.1)] bg-white">
+//       <h2 className="text-[18px] font-semibold mb-4">Адрес доставки</h2>
+
+//       {/* тут буде форма */}
+//     </div>
+//   );
+// }
