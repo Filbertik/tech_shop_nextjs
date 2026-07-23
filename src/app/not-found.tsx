7243bc268a83487cb439b9e8c45a5d1d1bdf4bf7
filbertik <aton2019zvit@gmail.com>
2026-07-23 12:37:35 +0300

    404 add content

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,23 +2,51 @@
 
 import { Button } from "@heroui/react";
 import Link from "next/link";
+import Image from "next/image";
 
-const NotFoundPage = () => {
+export default function NotFoundPage() {
   return (
-    <div className="flex flex-col items-center justify-center ">
-      <div className="text-8xl font-bold text-gray-300">404</div>
+    <div className="relative w-full min-h-[870px] bg-[#e4edf9] flex items-center justify-center overflow-hidden">
+      {/* LEFT BOTTOM IMAGE */}
+      <Image
+        src="/images/404/Group_l.png"
+        alt="left decor"
+        width={300}
+        height={300}
+        className="absolute bottom-0 left-0 pointer-events-none"
+      />
 
-      <h1 className="text-3xl font-bold tracking-tight">
-        Сторінки більше немає
-      </h1>
+      {/* RIGHT BOTTOM IMAGE */}
+      <Image
+        src="/images/404/Group_r.png"
+        alt="right decor"
+        width={300}
+        height={300}
+        className="absolute bottom-0 right-0 pointer-events-none"
+      />
 
-      <div className="pt-6">
-        <Button as={Link} color="primary" variant="shadow" href="/">
-          Повернутись на головну...
-        </Button>
+      {/* CENTER BLOCK */}
+      <div className="relative w-[547px] h-[585px] mt-[140px] mb-[144px] flex flex-col items-center">
+        {/* BACKGROUND IMAGE */}
+        <Image
+          src="/images/404/Group_404.png"
+          alt="404"
+          fill
+          className="object-contain pointer-events-none"
+        />
+
+        {/* TEXT */}
+        <p className="absolute top-[353px] text-[24px] font-semibold leading-[150%] text-[var(--accent)] text-center">
+          Цієї сторінки більше немає
+        </p>
+
+        {/* BUTTON */}
+        <div className="absolute top-[403px]">
+          <Button as={Link} href="/" color="primary" variant="shadow">
+            Повернутись на головну
+          </Button>
+        </div>
       </div>
     </div>
   );
-};
-
-export default NotFoundPage;
+}
