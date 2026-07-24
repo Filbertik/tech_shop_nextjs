b3c8645ae0624d1e2c0b8d9f17c5c3cd49c9398f
filbertik <aton2019zvit@gmail.com>
2026-07-24 11:00:58 +0300

    edit login

--- src/components/UI/modals/login.modal.tsx
+++ src/components/UI/modals/login.modal.tsx
@@ -6,14 +6,58 @@ import LoginForm from "@/forms/login.form";
 interface IProps {
   isOpen: boolean;
   onClose: () => void;
+  openRegister: () => void; // 👈 новий пропс
 }
 
-const LoginModal = ({ isOpen, onClose }: IProps) => {
+const LoginModal = ({ isOpen, onClose, openRegister }: IProps) => {
   return (
-    <CustomModal isOpen={isOpen} onClose={onClose} title="Авторизація">
+    <CustomModal
+      isOpen={isOpen}
+      onClose={onClose}
+      title="Авторизація"
+      className="bg-[#F5F5F7] rounded-[4px] w-[530px] h-[575px] px-[90px] py-[80px]"
+    >
+      {/* 🔁 Перемикач */}
+      <div className="flex justify-center mb-6">
+        <div className="flex border border-[var(--border-light)] rounded-[20px] p-[2px] w-[350px] h-[47px]">
+          {/* Вхід (активний) */}
+          <button className="rounded-[20px] px-[66px] h-[43px] bg-[#83A0F9] text-white">
+            Вхід
+          </button>
+
+          {/* Реєстрація */}
+          <button
+            onClick={openRegister}
+            className="rounded-[20px] px-[66px] h-[43px]"
+          >
+            Реєстрація
+          </button>
+        </div>
+      </div>
+
       <LoginForm onClose={onClose} />
     </CustomModal>
   );
 };
 
 export default LoginModal;
+
+// "use client";
+
+// import CustomModal from "@/components/common/modal";
+// import LoginForm from "@/forms/login.form";
+
+// interface IProps {
+//   isOpen: boolean;
+//   onClose: () => void;
+// }
+
+// const LoginModal = ({ isOpen, onClose }: IProps) => {
+//   return (
+//     <CustomModal isOpen={isOpen} onClose={onClose} title="Авторизація">
+//       <LoginForm onClose={onClose} />
+//     </CustomModal>
+//   );
+// };
+
+// export default LoginModal;
--- src/components/UI/modals/registration.modal.tsx
+++ src/components/UI/modals/registration.modal.tsx
@@ -6,14 +6,58 @@ import RegistrationForm from "@/forms/registration.form";
 interface IProps {
   isOpen: boolean;
   onClose: () => void;
+  openLogin: () => void; // 👈 новий пропс
 }
 
-const RegistrationModal = ({ isOpen, onClose }: IProps) => {
+const RegistrationModal = ({ isOpen, onClose, openLogin }: IProps) => {
   return (
-    <CustomModal isOpen={isOpen} onClose={onClose} title="Створити акаунт">
+    <CustomModal
+      isOpen={isOpen}
+      onClose={onClose}
+      title="Створити акаунт"
+      className="bg-[#F5F5F7] rounded-[4px] w-[530px] h-[653px] px-[90px] py-[80px]"
+    >
+      {/* 🔁 Перемикач */}
+      <div className="flex justify-center mb-6">
+        <div className="flex border border-[var(--border-light)] rounded-[20px] p-[2px] w-[350px] h-[47px]">
+          {/* Вхід */}
+          <button
+            onClick={openLogin}
+            className="rounded-[20px] px-[66px] h-[43px]"
+          >
+            Вхід
+          </button>
+
+          {/* Реєстрація (активна) */}
+          <button className="rounded-[20px] px-[38px] h-[43px] bg-[#83A0F9] text-white">
+            Реєстрація
+          </button>
+        </div>
+      </div>
+
       <RegistrationForm onClose={onClose} />
     </CustomModal>
   );
 };
 
 export default RegistrationModal;
+
+// "use client";
+
+// import CustomModal from "@/components/common/modal";
+// import RegistrationForm from "@/forms/registration.form";
+
+// interface IProps {
+//   isOpen: boolean;
+//   onClose: () => void;
+// }
+
+// const RegistrationModal = ({ isOpen, onClose }: IProps) => {
+//   return (
+//     <CustomModal isOpen={isOpen} onClose={onClose} title="Створити акаунт">
+//       <RegistrationForm onClose={onClose} />
+//     </CustomModal>
+//   );
+// };
+
+// export default RegistrationModal;
